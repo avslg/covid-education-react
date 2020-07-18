c716340545d98baf8edf07ba257d02250515a1cf
Artyom <artemisios2014@gmail.com>
2020-07-18 23:20:00 +1000

    new api service

--- src/App.js
+++ src/App.js
@@ -5,8 +5,23 @@ class App extends React.Component{
 
   getCovidJson = async (e) => {
     e.preventDefault();
-    
+    let country = "russia";
+    const xApiHost = "covid-193.p.rapidapi.com"; // https://rapidapi.com/api-sports/api/covid-193
+    const xApiKey = "ae61153825mshf5d7179ef888deap132741jsnd421646d8d62"; // CHANGE
+
+    let data = await  fetch(`https://covid-193.p.rapidapi.com/statistics?country=${country}`,{
+        "method": "GET",
+        "headers":{
+          "x-rapidapi-host": xApiHost,
+          "x-rapidapi-key": xApiKey
+        } 
+      });
+    let result = await data.json();
+    console.log(result);
     alert("invoked");
+
+
+
   }
 
   render(){
