5ff66d539585d2ce6aff27ef060cc7cb9378cb4c
Artyom <artemisios2014@gmail.com>
2020-07-30 19:30:55 +1000

    Updates

---
+++ precache-manifest.5292dd7db11d9cfb9e6d86a068cf5ee4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "78d3f6a990a709f1a57c7a7b253b0ef2",
+    "url": "/covid-education-react/index.html"
+  },
+  {
+    "revision": "fe9911b5913d2d127cb1",
+    "url": "/covid-education-react/static/css/main.abf8df91.chunk.css"
+  },
+  {
+    "revision": "033462f3b6f6164663d6",
+    "url": "/covid-education-react/static/js/2.b1aabbaf.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/covid-education-react/static/js/2.b1aabbaf.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fe9911b5913d2d127cb1",
+    "url": "/covid-education-react/static/js/main.d6310852.chunk.js"
+  },
+  {
+    "revision": "22a6ac2bb18c4ae174d5",
+    "url": "/covid-education-react/static/js/runtime-main.fbf3025e.js"
+  }
+]);
